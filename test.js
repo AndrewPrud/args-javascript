4166bc4bb7fc5a623906f6fcccc698a52a303180
Jihong <jihongbae2nd@gmail.com>
2022-06-15 13:53:51 -0500

    init changed

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+console.log("hello world");
+let width, height, lengthis, volume;
+
+width = parseInt(prompt("Enter width"));
+height = parseInt(prompt("Enter height"));
+lengthis = parseInt(prompt("Enter lenght"));
+
+volume = width * height * lengthis;
+
+console.log("volume:" + volume);
