fc6198377ac420401bfe32000a0e8b7718dffe5e
Aniket Singh <asingh11771@gmail.com>
2022-06-15 13:46:21 -0500

    Volume Problem

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+let height;
+let width;
+let depth;
+let volume;
+height = prompt("Enter Height");
+width = prompt("Enter Width");
+depth = prompt("Enter Depth");
+volume = height*width*depth;
+console.log(volume);
