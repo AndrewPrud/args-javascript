2b3986e38d92a774e9dee6284649787a857cfd7d
Aniruddh <aniruddhmayya@gmail.com>
2022-06-15 13:47:59 -0500

    It works.

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-console.log("Hello World");
+let length = parseInt(prompt("Length size: "));
+let width = parseInt(prompt("Width size: "));
+let height = parseInt(prompt("Height size: "));
+console.log(length * width * height);
