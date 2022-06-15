1d4f6e9885425e4bad4783c70353a062e39c6658
Aniket Singh <asingh11771@gmail.com>
2022-06-15 14:13:32 -0500

    Test Edit 2

--- Score.js
+++ Score.js
@@ -3,15 +3,15 @@ let score_2 = parseFloat(prompt("Enter Score 2"));
 let score_3 = parseFloat(prompt("Enter Score 3"));
 let average = (score_1+score_2+score_3)/3;
 if(average >= 92.5) {
-    console.log("A");
+    console.log( "Your Average is " + average +" which is an A");
 } else if (average < 92.5 && average >= 84.5) {
-    console.log("B");
+    console.log( "Your Average is " + average +" which is a B");
 } else if (average < 84.5 && average >= 77.5) {
-    console.log("C");
+    console.log( "Your Average is " + average +" which is a C");
 } else if (average < 77.5 && average >= 69.5) {
-    console.log("D");
+    console.log( "Your Average is " + average +" which is a D");
 } else if (average <= 69.5) {
-    console.log("F");
+    console.log( "Your Average is " + average +" which is a F");
 }
 
 
