e230176c5f0b151aa608f3acf91fc02211f09e0a
Aniruddh <aniruddhmayya@gmail.com>
2022-06-16 09:12:29 -0500

    Added comments

--- longName.js
+++ longName.js
@@ -1,9 +1,11 @@
+//Getting names from the user and adding them to an array
 let numNames = parseInt(prompt("Enter number of names: "));
 const nameList = [];
 for(var i = 0; i < numNames; i++){
     nameList.push(prompt("Enter full name: "));
 }
 
+//traversing the array to find the longest last name and outputting it
 let longestName;
 let longestSize = 0;
 for(var i = 0; i < nameList.length; i++){
@@ -15,6 +17,7 @@ for(var i = 0; i < nameList.length; i++){
 }
 console.log(longestName);
 
+//finding the shortest last name in order to output the range in lengths of last names
 let shortestSize = longestSize;
 for(var j = 0; j < nameList.length; j++){
     thisSize = nameList[j].substring(nameList[j].indexOf(" ")).length;
@@ -25,6 +28,7 @@ for(var j = 0; j < nameList.length; j++){
 
 console.log(longestSize - shortestSize);
 
+//outputting all the names in the list
 for(var k = 0; k < nameList.length; k++){
     console.log(nameList[k]);
 }
