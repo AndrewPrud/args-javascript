1ad0399dd9f76800f1565d66fe52138073b91e8f
Aniruddh <aniruddhmayya@gmail.com>
2022-06-16 10:47:51 -0500

    Added functions

--- longName.js
+++ longName.js
@@ -1,3 +1,31 @@
+function getLongest(xList){
+    //traversing the array to find the longest last name and outputting it
+    let longestName;
+    let longestSize = 0;
+    for(var i = 0; i < xList.length; i++){
+        thisSize = xList[i].substring(xList[i].indexOf(" ")).length;
+        if(thisSize > longestSize){
+            longestSize = thisSize;
+            longestName = xList[i];
+        }
+    }
+    return longestName;
+    }
+
+
+function getRange(yList){
+//finding the shortest last name in order to output the range in lengths of last names
+let shortestSize = getLongest(yList).substring(getLongest(yList).indexOf(" ")).length;
+for(var j = 0; j < yList.length; j++){
+    thisSize = yList[j].substring(yList[j].indexOf(" ")).length;
+    if(thisSize < shortestSize){
+        shortestSize = thisSize;
+    }
+}
+return getLongest(yList).substring(getLongest(yList).indexOf(" ")).length - shortestSize;
+}
+
+
 //Getting names from the user and adding them to an array
 let numNames = parseInt(prompt("Enter number of names: "));
 const nameList = [];
@@ -5,28 +33,9 @@ for(var i = 0; i < numNames; i++){
     nameList.push(prompt("Enter full name: "));
 }
 
-//traversing the array to find the longest last name and outputting it
-let longestName;
-let longestSize = 0;
-for(var i = 0; i < nameList.length; i++){
-    thisSize = nameList[i].substring(nameList[i].indexOf(" ")).length;
-    if(thisSize > longestSize){
-        longestSize = thisSize;
-        longestName = nameList[i];
-    }
-}
-console.log(longestName);
+console.log(getLongest(nameList));
+console.log(getRange(nameList));
 
-//finding the shortest last name in order to output the range in lengths of last names
-let shortestSize = longestSize;
-for(var j = 0; j < nameList.length; j++){
-    thisSize = nameList[j].substring(nameList[j].indexOf(" ")).length;
-    if(thisSize < shortestSize){
-        shortestSize = thisSize;
-    }
-}
-
-console.log(longestSize - shortestSize);
 
 //outputting all the names in the list
 for(var k = 0; k < nameList.length; k++){
